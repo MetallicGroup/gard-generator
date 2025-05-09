ea58e88eea2613204e2c474ebcb5a88087aadbcb
MetallicGroup <marketing@metallicroof.ro>
2025-05-09 11:05:38 +0300

    Update and rename main.py to app.py

--- app.py
+++ app.py
@@ -46,3 +46,7 @@ def process():
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+        if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render setează portul dinamic
+    app.run(host="0.0.0.0", port=port)
+
