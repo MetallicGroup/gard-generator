d06588f41afb63c3c86f4d940dc8d345fcb9b610
MetallicGroup <marketing@metallicroof.ro>
2025-05-12 14:41:29 +0300

    Update main.py
    git add main.py
    git commit -m "Adaugă print server startup"
    git push

--- main.py
+++ main.py
@@ -100,4 +100,7 @@ if __name__ == "__main__":
     from waitress import serve
     import os
     port = int(os.environ.get("PORT", 5000))
+
+    print("🚀 Serverul pornește pe port", port, flush=True)  # 👈 Asta e linia nouă
+
     serve(app, host="0.0.0.0", port=port)
